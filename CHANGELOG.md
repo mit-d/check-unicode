82f8f757f9529ed3a9b6a28bca7988262de3ebf3
mit-d <derekmttn@gmail.com>
2026-02-28 12:47:22 -0700

    release: v0.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 0.4.0 - 2026-02-28
 
 ### Added
 
--- src/check_unicode/__init__.py
+++ src/check_unicode/__init__.py
@@ -1,3 +1,3 @@
 """Pre-commit hook to detect and fix non-ASCII Unicode characters."""
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
